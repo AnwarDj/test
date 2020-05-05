2e64858920d07dd2557858c5b11ffde49bf924d3
AnwarDj <34960210+AnwarDj@users.noreply.github.com>
2020-05-05 12:39:06 +0100

    first commit

---
+++ service.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="uft-8" >
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>service </title>
+<link rel="stylesheet" href="css/bootstrap-rtl.css" >
+<link rel="stylesheet" href="css/bootstrap.min.css" >
+<link rel="stylesheet" href="css/fontawesome.css" >
+<link rel="stylesheet" href="css/style.css" >
+</head>
+
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+    <ul class="nav navbar-nav navbar-right">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"><i class="fas fa-home"></i>  الرئيــــسية <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          اسم المستخدم
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#"><i class="fas fa-user-circle">  الصفحة الـــشخصية </i></a>
+          <a class="dropdown-item" href="#"><i class="fas fa-user-edit"></i>  تعديل الصفحة الشخصية </a>
+          <div class="dropdown-divider"></div>
+          <a href="logout.php" class="dropdown-item" ><i class="fas fa-sign-out-alt"></i>  تسجيل الـــــخروج </a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-comments"></i>  الرســـــائل</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-bell"></i>  الإشعــــارات</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="search" placeholder="إبحث بالإسم" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">البحث <i class="fas fa-search"></i></button>
+    </form>
+    <a class="navbar-brand" href="#" >خدمــــــــــــــة</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  </div>
+</nav>
+<script src= "js/jquery-3.2.0.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ 
+ </body>
+ </html>
